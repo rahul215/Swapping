4286dd557180a030180611ba13d84ecaf5b674f7
rahul215 <rahul.saxena_cs18@gla.ac.in>
2020-01-13 23:49:58 +0530

    Initial commit

---
+++ src/Swapping.java
@@ -0,0 +1,12 @@
+public class Swapping {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 27;
+        int temp;
+        System.out.println("Before swapping:- = "+a+", "+ + b);
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After Swapping:-  =  "+a+" , "+ +b);
+    }
+}
